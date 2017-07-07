728c5ad7417834307492f4ade11aeb8d6f403034
Jon Kemp <jonkemp12@gmail.com>
2017-07-07 15:54:30 +0100

    Config to run webpack on heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 var webpack = require('webpack');
 var path = require('path');
 
+// Get node environment for production or dev:
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+
 module.exports = {
   entry: [
     'script!jquery/dist/jquery.min.js',
@@ -14,6 +18,12 @@ module.exports = {
     new webpack.ProvidePlugin({
       '$': 'jquery',
       'jQuery': 'jquery'
+    }),
+    // prevent warnings in optimize compressor
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
     })
   ],
   output: {
@@ -55,5 +65,6 @@ module.exports = {
       path.resolve(__dirname, './node_modules/foundation-sites/scss')
     ]
   },
-  devtool: 'cheap-module-eval-source-map'
+  // Dont generate source maps in production:
+  devtool: process.env.NODE_ENV === 'production' ? undefined : 'cheap-module-eval-source-map'
 };
